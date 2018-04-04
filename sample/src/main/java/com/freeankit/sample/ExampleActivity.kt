ed751d3a2a9836b87703808836cfe6e60037ebc2
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-04-04 14:52:53 +0800

    Added RoundFrameLayout sample

---
+++ sample/src/main/java/com/freeankit/sample/ExampleActivity.kt
@@ -0,0 +1,15 @@
+package com.freeankit.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 04/04/2018 (MM/DD/YYYY)
+ */
+class ExampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_round_framelayout)
+    }
+}
--- sample/src/main/java/com/freeankit/sample/MainActivity.kt
+++ sample/src/main/java/com/freeankit/sample/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.freeankit.sample
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startConstraintActivity(view: View) {
+        startActivity(Intent(this@MainActivity, ExampleActivity::class.java))
+    }
+
+    fun startLinearActivity(view: View) {
+        startActivity(Intent(this@MainActivity, ExampleActivity::class.java))
+    }
+
+    fun startRelativeActivity(view: View) {
+        startActivity(Intent(this@MainActivity, ExampleActivity::class.java))
+    }
+
+    fun startFrameActivity(view: View) {
+        startActivity(Intent(this@MainActivity, ExampleActivity::class.java))
     }
 }
